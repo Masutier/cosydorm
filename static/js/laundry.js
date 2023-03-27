9e5b42cbcf01138a2b7d3d3181a7661e23be7c11
Masutier <masutier@gmail.com>
2023-03-26 19:02:59 -0500

    change icons in main page

--- static/js/laundry.js
+++ static/js/laundry.js
@@ -1,13 +1,13 @@
 
-ScrollReveal({reset: true, distance: '160px', duration: 2500, delay: 400});
+ScrollReveal({reset: true, distance: '160px', duration: 750, delay: 300});
 
-ScrollReveal().reveal('.main-title', { delay: 300, origin: 'left' });
-ScrollReveal().reveal('.sec-01 .image', { delay: 400, origin: 'bottom' });
-ScrollReveal().reveal('.text-box', { delay: 500, origin: 'right' });
+ScrollReveal().reveal('.main-title', { delay: 200, origin: 'left' });
+ScrollReveal().reveal('.sec-01 .image', { delay: 200, origin: 'bottom' });
+ScrollReveal().reveal('.text-box', { delay: 100, origin: 'right' });
 
-ScrollReveal().reveal('.sec-02 .image', { delay: 300, origin: 'left' });
-ScrollReveal().reveal('.info', { delay: 300, origin: 'right' });
+ScrollReveal().reveal('.sec-02 .image', { delay: 100, origin: 'left' });
+ScrollReveal().reveal('.info', { delay: 100, origin: 'right' });
 
-ScrollReveal().reveal('.sec-03 .image', { delay: 300, origin: 'left' });
+ScrollReveal().reveal('.sec-03 .image', { delay: 100, origin: 'left' });
 
-ScrollReveal().reveal('.sec-04 .image', { delay: 300, origin: 'left' });
+ScrollReveal().reveal('.sec-04 .image', { delay: 100, origin: 'left' });
